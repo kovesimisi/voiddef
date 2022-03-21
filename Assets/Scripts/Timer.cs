9fc5b02f5e231decbd10a44dcac405cc0e3386d6
unknown <eoh12i@inf.elte.hu>
2022-03-21 15:23:06 +0100

    Added timer script

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//USEAGE: Give needed time, and function as parameter; time in seconds, function without "()"
+
 public class Timer : MonoBehaviour
 {
+    [SerializeField] 
+    float cDFrom = 0.0f; //countdown from
+
+    [SerializeField]
+    string functionToCall = "";
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,13 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (cDFrom > 0)
+        {
+            cDFrom -= Time.deltaTime;
+        }
+        else 
+        {
+            gameObject.SendMessage(functionToCall);
+        }
     }
 }
