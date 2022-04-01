1b59bc8e5fa920da9d3bb66df57acfc6ec7236f6
Erdős Csanád <e6v1nq@inf.elte.hu>
2022-04-01 15:39:07 +0200

    Setup CI pipeline

--- scripts/util/viewport_config.gd
+++ scripts/util/viewport_config.gd
@@ -4,3 +4,7 @@ func _ready():
 	fxaa = GraphicsSettings.fxaa
 	msaa = GraphicsSettings.msaa
 	shadow_atlas_size = GraphicsSettings.shadow_resolution
+	shadow_atlas_quad_0 = Viewport.SHADOW_ATLAS_QUADRANT_SUBDIV_1
+	shadow_atlas_quad_1 = Viewport.SHADOW_ATLAS_QUADRANT_SUBDIV_4
+	shadow_atlas_quad_2 = Viewport.SHADOW_ATLAS_QUADRANT_SUBDIV_4
+	shadow_atlas_quad_3 = Viewport.SHADOW_ATLAS_QUADRANT_SUBDIV_16
