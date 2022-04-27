f2a8dca57d1f4334b0cb5b61b440a7dee28b21f5
Erdős Csanád <e6v1nq@inf.elte.hu>
2022-04-27 18:37:26 +0200

    Fix unit test leaks

--- test/unit/test_castle.gd
+++ test/unit/test_castle.gd
@@ -6,17 +6,16 @@ func before_all():
 func test_castle_hit():
 	var res = load('res://objects/Castle.tscn')
 	var castle = res.instance()
-	add_child(castle)
+	add_child_autofree(castle)
 	castle.hp = 2
 	assert_eq(2, castle.hp)
 	castle.hit()
 	assert_eq(1, castle.hp)
-	castle.free()
 	
 func test_castle_gameover():
 	var res = load('res://objects/Castle.tscn')
 	var castle = res.instance()
-	add_child(castle)
+	add_child_autofree(castle)
 	castle.hp = 1
 	assert_eq(1, castle.hp)
 	castle.hit()
@@ -27,25 +26,30 @@ func test_castle_spawn_unit():
 	var castle = res.instance()
 	var enemy_castle = res.instance()
 	enemy_castle.team_id = 1
-	add_child(castle)
-	add_child(enemy_castle)
+	add_child_autofree(castle)
+	add_child_autofree(enemy_castle)
 	var t = castle.spawn_unit(2)
-	assert_true(is_instance_valid(t))
+	assert_true(t)
+	
+	$Unit.free()
+	# t.free()
 
 func test_castle_spawn_tower():
 	var res = load('res://objects/Castle.tscn')
 	var castle = res.instance()
-	add_child(castle)
+	add_child_autofree(castle)
 	var t = castle.spawn_tower(Vector3.ZERO, 0)
 	assert_true(t)
-	pass;
+	castle.towers[0].free()
 	
 func test_castle_spawn_tower_overload():
 	var res = load('res://objects/Castle.tscn')
 	var castle = res.instance()
-	add_child(castle)
+	add_child_autofree(castle)
 	castle.max_towers = 1
 	var t = castle.spawn_tower(Vector3.ZERO, 0)
 	assert_true(t)
 	var t2 = castle.spawn_tower(Vector3.ZERO, 0)
 	assert_true(!t2)
+	
+	castle.towers[0].free()
