2ee51afd0cc2ef0783871df34dd1b86c66ed13cc
viki <l9z49i@inf.elte.hu>
2022-04-27 14:59:25 +0200

    unit tests2

--- test/unit/test_tower.gd
+++ test/unit/test_tower.gd
@@ -13,13 +13,14 @@ func test_tower_hit():
 func test_tower_die_on_zero_hp():
 	var castleRes = load('res://objects/Castle.tscn')
 	var castle = castleRes.instance()
+	yield(get_tree(), "idle_frame")
 	var res = load('res://objects/Tower.tscn')
 	var tower = res.instance()
 	castle.towers.append(tower)
 	add_child(tower)
 	castle.towers[0].hp = 1
-	assert_eq(GameManager.castles[1], castle)
+	castle.towers[0].hit()
 	assert_eq(castle.team_id, 0)
 	
-	yield(get_tree(), "idle_frame")
+	
 	assert_freed(castle.towers[0], "")
