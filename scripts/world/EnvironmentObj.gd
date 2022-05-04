ce2b01b69b7f3cb875ad71ceae9b87488a0ce96b
viki <l9z49i@inf.elte.hu>
2022-05-04 08:28:15 +0200

    added environment object

---
+++ scripts/world/EnvironmentObj.gd
@@ -0,0 +1,23 @@
+extends KinematicBody
+
+var type = "environment_obj"
+
+var target : Spatial
+export var hp = 2
+
+onready var healthbar = $Healthbar
+
+var max_hp : int
+
+func _ready():
+	$Mesh.set_surface_material(0,GameManager.unit_materials[Color(1,0.1,0.1)])
+	healthbar.value = 1.0
+	max_hp = hp
+
+
+func hit(dmg = 1):
+	hp -= dmg
+	healthbar.value = float(hp) / max_hp
+	
+	if hp == 0:
+		queue_free()
